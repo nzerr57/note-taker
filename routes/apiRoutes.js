0cf966e08b3adc111991c2a7bf32ca82a6d0d485
NZerr <nathan.zerr@gmail.com>
2021-06-15 19:09:02 -0400

    notes can now be entered/saved

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,32 @@
 
-const noteData = require('../data/noteData');
+const fs = require('fs');
+const db = require('../db/db.json');
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(noteData));
+    app.get('/api/notes', (req, res) => {
+        fs.readFile('./db/db.json','utf8', (err, data) => {
+            if (err) throw err;
+            const noteData = JSON.parse(data)
+            res.json(noteData) 
+        })
+       
+    });
+    
 
     app.post('/api/notes', (req, res) => {
-        noteData.push(req.body);
+        let newNote = req.body;
+        console.log(newNote);
+        fs.readFile('./db/db.json','utf8', (err, data) => {
+            if (err) throw err;
+            const allNotes = JSON.parse(data)
+            newNote.id = allNotes.length+1
+            console.log(newNote);
+            allNotes.push(newNote)
+
+            fs.writeFile('./db/db.json', JSON.stringify(allNotes), (err, data) => {
+                if (err) throw err;
+                res.json(allNotes)
+            })
+        })
     });
 }
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 //require('./routes/apiRoutes')(app);
+require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
+
+
 
 app.listen(PORT, () => {
     console.log(`App listening on Port: ${PORT}`);
