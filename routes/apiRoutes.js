574c237f4b71148910902865783462258fab7e0b
NZerr <nathan.zerr@gmail.com>
2021-06-15 21:04:32 -0400

    added functionality to delete notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,14 @@
 
 const fs = require('fs');
 const db = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
         fs.readFile('./db/db.json','utf8', (err, data) => {
             if (err) throw err;
-            const noteData = JSON.parse(data)
-            res.json(noteData) 
+            const noteData = JSON.parse(data);
+            res.json(noteData); 
         })
        
     });
@@ -18,15 +19,23 @@ module.exports = (app) => {
         console.log(newNote);
         fs.readFile('./db/db.json','utf8', (err, data) => {
             if (err) throw err;
-            const allNotes = JSON.parse(data)
-            newNote.id = allNotes.length+1
+            const allNotes = JSON.parse(data);
+            newNote.id = uuidv4();
             console.log(newNote);
-            allNotes.push(newNote)
+            allNotes.push(newNote);
 
             fs.writeFile('./db/db.json', JSON.stringify(allNotes), (err, data) => {
                 if (err) throw err;
-                res.json(allNotes)
+                res.json(allNotes);
             })
         })
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+        const notes = data.filter((note) => note.id !== id);
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes), "utf-8");
+        res.send("Note Removed");
     });
 }
